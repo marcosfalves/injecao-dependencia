a04b7005669e8d7acdefd8d187551446fcd36857
Marcos F. Alves <marcosf_alves@outlook.com>
2022-03-16 22:00:12 -0300

    Criação Projeto

---
+++ src/main/java/com/mfalves/spring/PraticaInjecaoDependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.mfalves.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaInjecaoDependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaInjecaoDependenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfalves/spring/PraticaInjecaoDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfalves.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaInjecaoDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
