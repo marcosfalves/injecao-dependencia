1a9751a4b1bdb81b741b9fc42b0c06bd0950ff17
Marcos F. Alves <marcosf_alves@outlook.com>
2022-03-27 09:18:56 -0300

    Pontos de injeção

--- src/main/java/com/mfalves/spring/domain/service/AtivacaoClienteService.java
+++ src/main/java/com/mfalves/spring/domain/service/AtivacaoClienteService.java
@@ -1,21 +1,34 @@
 package com.mfalves.spring.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.mfalves.spring.domain.model.Cliente;
 import com.mfalves.spring.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 	
+	@Autowired
 	private Notificador notificador;
-		
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		System.out.println("Construtor de AtivacaoClienteService: " + notificador);
-	}
+	
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+	
+//	public AtivacaoClienteService(String qualquerCoisa) {
+//		
+//	}
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
-		
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
+
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}	
 
 }
