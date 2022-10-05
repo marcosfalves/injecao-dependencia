d0ddb4b2729a127daa278ece49e45a19cf81560f
Marcos F. Alves <marcosf_alves@outlook.com>
2022-10-05 08:08:00 -0300

    Criando métodos de callback do ciclo de vida dos beans

--- src/main/java/com/mfalves/spring/domain/service/AtivacaoClienteService.java
+++ src/main/java/com/mfalves/spring/domain/service/AtivacaoClienteService.java
@@ -1,5 +1,7 @@
 package com.mfalves.spring.domain.service;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +10,26 @@ import com.mfalves.spring.notificacao.NivelUrgencia;
 import com.mfalves.spring.notificacao.Notificador;
 import com.mfalves.spring.notificacao.TipoNotificador;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-public class AtivacaoClienteService {
+public class AtivacaoClienteService implements InitializingBean, DisposableBean {
 
 	@TipoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+
+	//@PostConstruct
+	public void init(){
+		System.out.println("INIT " + notificador);
+	}
+
+	//@PreDestroy
+	@Override
+	public void destroy(){
+		System.out.println("DESTROY");
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
@@ -21,4 +37,8 @@ public class AtivacaoClienteService {
 	}
 
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("INIT " + notificador);
+	}
 }
