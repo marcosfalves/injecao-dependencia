2741d30c5b431926949ea095b9a31c126cf979e8
Marcos F. Alves <marcosf_alves@outlook.com>
2022-10-05 08:15:16 -0300

    Preparando o código para pŕoxima aula.

--- src/main/java/com/mfalves/spring/domain/service/AtivacaoClienteService.java
+++ src/main/java/com/mfalves/spring/domain/service/AtivacaoClienteService.java
@@ -1,44 +1,21 @@
 package com.mfalves.spring.domain.service;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.mfalves.spring.domain.model.Cliente;
 import com.mfalves.spring.notificacao.NivelUrgencia;
 import com.mfalves.spring.notificacao.Notificador;
 import com.mfalves.spring.notificacao.TipoNotificador;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
-public class AtivacaoClienteService implements InitializingBean, DisposableBean {
+public class AtivacaoClienteService {
 
 	@TipoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
 
-	//@PostConstruct
-	public void init(){
-		System.out.println("INIT " + notificador);
-	}
-
-	//@PreDestroy
-	@Override
-	public void destroy(){
-		System.out.println("DESTROY");
-	}
-
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-	}
-
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		System.out.println("INIT " + notificador);
 	}
 }
--- src/main/java/com/mfalves/spring/notificacao/NotificadorEmail.java
+++ src/main/java/com/mfalves/spring/notificacao/NotificadorEmail.java
@@ -1,23 +1,15 @@
 package com.mfalves.spring.notificacao;
 
-import org.springframework.context.annotation.Profile;
+import com.mfalves.spring.domain.model.Cliente;
 import org.springframework.stereotype.Component;
 
-import com.mfalves.spring.domain.model.Cliente;
-
-@Profile("prod")
 @TipoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
-
-	public NotificadorEmail() {
-		System.out.println("Notificador E-mail REAL");
-	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando o cliente %s atráves do e-mail %s  Mensagem: %s\n", cliente.getNome(),
 				cliente.getEmail(), mensagem);
 	}
-
 }
