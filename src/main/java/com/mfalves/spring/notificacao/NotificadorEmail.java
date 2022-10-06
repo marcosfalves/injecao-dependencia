e3a4cba7118065b88e90e52fd723c0c82a7a0ef6
Marcos F. Alves <marcosf_alves@outlook.com>
2022-10-06 08:20:24 -0300

    Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/mfalves/spring/notificacao/NotificadorEmail.java
+++ src/main/java/com/mfalves/spring/notificacao/NotificadorEmail.java
@@ -1,14 +1,24 @@
 package com.mfalves.spring.notificacao;
 
 import com.mfalves.spring.domain.model.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @TipoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
 
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
+
 		System.out.printf("Notificando o cliente %s atráves do e-mail %s  Mensagem: %s\n", cliente.getNome(),
 				cliente.getEmail(), mensagem);
 	}
